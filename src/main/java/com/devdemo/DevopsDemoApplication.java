14dcac3c5934570e33aded843f6f57c13e71be4a
Vittal777 <vittalsai278@gmail.com>
2026-01-16 22:07:13 +0530

    Initial Commit - Devops Demo

---
+++ src/main/java/com/devdemo/DevopsDemoApplication.java
@@ -0,0 +1,25 @@
+package com.devdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+	@GetMapping("/")
+    public String home() {
+        return "Hello from DevOps App v1";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "UP";
+    }
+
+}
---
+++ src/test/java/com/devdemo/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
